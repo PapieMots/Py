14eef231a6f91ef9e1be09331392a45771939bd8
Ntando Mngomezulu <ntendo64@gmail.com>
2020-03-29 09:02:18 +0100

    Add DB functionality to Luno Script

--- Luno.py
+++ Luno.py
@@ -2,57 +2,86 @@
 import requests
 import time
 import datetime
-from openpyxl import load_workbook
+#from openpyxl import load_workbook
 from luno_python.client import Client
+import mysql.connector as mariadb
+from mysql.connector import errorcode
 
-#####################################################################
-# Make connection to Luno server
-#####################################################################
+#########################
+# Initiate DB connection
+#########################
 
-c = Client(api_key_id="cu8mryhtp5efk", api_key_secret="OmvIryVwHWYe9vaEqDLg4kJtXYXVFTLw8nwx-ng-x_Y")
-#c = Client(api_key_id="cu8mryhtp5efk", api_key_secret="1yW7o-KuwgkBfmRz_LBJFMeL8lzp070slIVLjrOqVaE")
+try:
+    cnx = mariadb.connect(user='pythonuser', password='pythonuserpassword',
+                              host='arbitrage.hopto.org',
+                              database='arbitrage')
+except mariadb.Error as err:
+    if err.no == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Authentication Error")
+    elif err.no == errorcode.ER_BAD_DB_ERROR:
+        print("Non-existent DB")
+    else:
+        print(err)
+else:
+    # Set DB Cursor
+    cursor = cnx.cursor()
+    
+    #####################################################################
+    # Make connection to Luno server
+    #####################################################################
 
-#####################################################################
-# Get tickers
-#####################################################################
+    c = Client(api_key_id="cu8mryhtp5efk", api_key_secret="OmvIryVwHWYe9vaEqDLg4kJtXYXVFTLw8nwx-ng-x_Y")
+    #c = Client(api_key_id="cu8mryhtp5efk", api_key_secret="1yW7o-KuwgkBfmRz_LBJFMeL8lzp070slIVLjrOqVaE")
 
-res = c.get_ticker("XBTZAR")
+    #####################################################################
+    # Get tickers
+    #####################################################################
 
-#####################################################################
-# Get balances
-# - Commented the below out due to unhandled exceptions
-# - We will have to check this out when we begin actual trades
-#####################################################################
+    res = c.get_ticker("XBTZAR")
 
-#balance = c.get_balances()
+    #####################################################################
+    # Get balances
+    # - Commented the below out due to unhandled exceptions
+    # - We will have to check this out when we begin actual trades
+    #####################################################################
 
-#####################################################################
-# Show balances
-#####################################################################
+    #balance = c.get_balances()
 
-# print(balance)
-print(res)       #Debug data
+    #####################################################################
+    # Show balances
+    #####################################################################
 
-# parse XBTZAR
-for key,value in res.items():
-    if key == 'timestamp':
-       luno_timestamp_epoch = value
-       #TO-DO:epoch time conversion for writing to xls
-    if key == 'ask':
-       luno_askprice = value
-       
-# Add Indicators to Sheet
-wb = load_workbook(filename = 'DATE Check.xlsx')
-ws = wb['LunoData']
-ws.insert_rows(2)
-#ws.cell(2,1,luno_timestamp_epoch)
-ws.cell(2,1,time.strftime('%Y-%m-%dT%H:%M:%S %Z',time.localtime(time.time())))
-ws.cell(2,2,luno_askprice)
-wb.save(filename = 'DATE Check.xlsx')
+    # print(balance)
+    print(res)       #Debug data
+
+    # parse XBTZAR
+    for key,value in res.items():
+        if key == 'timestamp':
+           luno_timestamp_epoch = value
+           #TO-DO:epoch time conversion for writing to xls
+        if key == 'rolling_24_hour_volume':
+           luno_volume = value   
+        if key == 'ask':
+           luno_askprice = value
+    luno_currency = 'bitcoin'
+    luno_pair = 'btczar'
+    cursor.execute("INSERT INTO luno (id,currency,pair,price,volume) VALUES (%s,%s,%s,%s,%s)", (luno_timestamp_epoch,luno_currency,luno_pair,luno_askprice,luno_volume))
+    
+    cnx.commit()
+    cursor.close()
+    cnx.close()            
+    # Add Indicators to Sheet
+    #wb = load_workbook(filename = 'DATE Check.xlsx')
+    #ws = wb['LunoData']
+    #ws.insert_rows(2)
+    ##ws.cell(2,1,luno_timestamp_epoch)
+    #ws.cell(2,1,time.strftime('%Y-%m-%dT%H:%M:%S %Z',time.localtime(time.time())))
+    #ws.cell(2,2,luno_askprice)
+    #wb.save(filename = 'DATE Check.xlsx')
 
 
-# try:
-#    res = c.get_ticker(pair='XBTZAR')
-#    print(res)
-# except Exception as e:
-#   print (e)
+    # try:
+    #    res = c.get_ticker(pair='XBTZAR')
+    #    print(res)
+    # except Exception as e:
+    #   print (e)
