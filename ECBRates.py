76287fb4c2700d517649baf2641b9b9393a13a02
PapieMots <58475489+PapieMots@users.noreply.github.com>
2020-04-20 17:29:49 +0200

    Update ECBRates.py

--- ECBRates.py
+++ ECBRates.py
@@ -1,14 +1,24 @@
-# Import relevant libraries
+# Import libraries
 import requests
-import time
+import date
 import datetime
 import json
 
-# Pull EURZAR Exchange Rate from ECB
-exchangerates = requests.get('https://api.exchangeratesapi.io/latest')
-exchangerates_json = exchangerates.json()
-rates = exchangerates_json['rates']
+# Pull exchange rate from ExchangeRatesApi
+url = 'https://api.exchangeratesapi.io/latest'
+exchangerate = requests.get(url, params={'base':'USD'})
+exchangerate_json = exchangerate.json()
 
-# debug
-print("Printing price information vertically")
-print(rates)
+# Assign relevant values to variables
+Prices = exchangerates_json['rates']
+ZARPrice = Prices['ZAR']
+USDPrice = Prices['USD']
+GBPPrice = Prices['GBP'] 
+BaseCCY = exchangerates_json['base']
+
+# Debug printing
+print('ZAR Price :', ZARPrice)
+print('USD Price :', USDPrice)
+print('GBP Price: ', GBPPrice)
+print('Base  CCY:', BaseCCY)
+
